677446810e06c81bf087bd7bebd7f0f4d99b6ea9
Megan <megan.wood.cs@gmail.com>
2026-02-08 02:45:41 -0800

    Gallery component created

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,20 +1,49 @@
-
+import { useRef, useEffect } from "react";
 import Image from "./Image.tsx";
 
+interface GalleryImage {
+  src: string;
+  alt?: string; 
+  caption?: string; 
+}
+
 interface GalleryProps {
-  images: typeof Image[]
+  images: GalleryImage[];
 }
 
 export default function Gallery({ images }: GalleryProps) {
+  const galleryRef = useRef<HTMLDivElement>(null); 
+
+  useEffect(() => {
+    const gallery = galleryRef.current;
+    if (!gallery) return; 
+
+    const onWheel = (e: WheelEvent) => {
+      if (Math.abs(e.deltaY) > Math.abs(e.deltaX)) {
+        e.preventDefault(); 
+        gallery.scrollLeft += e.deltaY * 1.2;
+      }
+    };
+
+    gallery.addEventListener("wheel", onWheel, { passive: false });
+    return () => gallery.removeEventListener("wheel", onWheel);
+  }, []); 
+
+  
+
+  
   return (
     <>
-      <div className="gallery">
-        <Image src="SR-vio-final2.jpg" alt="graphic for 100th anniversary of Santa Rosa Symphony with violin"/>
-        <Image src="SR-piano-final2.jpg" alt="graphic for 100th anniversary of Santa Rosa Symphony with piano"/>
-        <Image src="RC_LONG53-web.jpg" alt="graphic for farro from Italian Harvest"/>
-        <Image src="RC_LONG53-web.jpg" alt="graphic for farro from Italian Harvest"/>
-        <Image src="RC_LONG53-web.jpg" alt="graphic for farro from Italian Harvest"/>
+      <div className="gallery" ref={galleryRef}>
+        {images.map((img, index) => (
+          <Image 
+            key={index}
+            src={img.src}
+            alt={img.alt}
+            {...(img.caption ? { caption: img.caption } : {})}
+          />
+        ))}
       </div>
     </>
-  )
+  );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,50 @@
 
 import Navbar from "../components/Navbar";
 import Image from "../components/Image";
+import Gallery from "../components/Gallery";
 
 export default function Home() {
+  const images = [
+    {
+      src: "SR-vio-final2.jpg",
+      alt: "graphic for 100th anniversary of Santa Rosa Symphony with violin"
+    },
+    {
+      src: "RC_LONG53-web.jpg",
+      alt: "graphic for risotto from Italian Harvest"
+    },
+    {
+      src: "Jersey-front-portfolio-web.jpg",
+      alt: "graphic for Asti Tour de Vine"
+    },
+    {
+      src: "final_3_violin_x-web.jpg",
+      alt: "graphic for LHASA Concert with violin"
+    },
+    {
+      src: "MM_ALT_postcard_front_2_Artboard_4_copy-web.jpg",
+      alt: "graphic for market match"
+    }, 
+    {
+      src: "Final_Snellen_RATS_hirez-art.png", 
+      alt: "graphic for Year of the Rat 2020"
+    }
+  ]
+
+
   return (
     <>
       <section id="homePage">
         <Navbar/>
+        <Gallery images={images}/>
         {/* <div className="galleryWrapper"> */}
-          <div className="gallery">
+          {/* <div className="gallery">
             <Image src="SR-vio-final2.jpg" alt="graphic for 100th anniversary of Santa Rosa Symphony with violin"/>
             <Image src="SR-piano-final2.jpg" alt="graphic for 100th anniversary of Santa Rosa Symphony with piano"/>
+            <Image src="RC_LONG53-web.jpg" alt="graphic for risotto from Italian Harvest"/>
             <Image src="RC_LONG53-web.jpg" alt="graphic for farro from Italian Harvest"/>
             <Image src="RC_LONG53-web.jpg" alt="graphic for farro from Italian Harvest"/>
-            <Image src="RC_LONG53-web.jpg" alt="graphic for farro from Italian Harvest"/>
-          </div>
+          </div> */}
         {/* </div> */}
       </section>
     </>
